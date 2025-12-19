124c29841394b2d465cdd7a3f89f18f4122fe60f
zzrawan <s145221@student.squ.edu.om>
2025-12-19 23:40:47 +0400

    Update Restaurant.php
    include the connection with db.php

--- Web_project/Restaurant.php
+++ Web_project/Restaurant.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 
+    
     <head>
-
+        
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
         <title>SQU Student Campus Housing - Restaurant</title>
+        <?php include "db.php";
+        ?>
 
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
