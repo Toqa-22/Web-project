b396b07d2c0e3cd9f9bd76ecd97eee1f29980f63
Toqa AL-Sinawi <toqaalsinawi@gmail.com>
2025-12-20 07:35:08 -0800

    Update db.php

--- Web_project/db.php
+++ Web_project/db.php
@@ -3,9 +3,7 @@
     $username = "root";
     $password = "";
     $dbname = "comp3700";
-
     $conn = mysqli_connect($servername, $username, $password, $dbname);
-
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
